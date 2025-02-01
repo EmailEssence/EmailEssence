71cb1c0a3c284d734ae4a5029203b94cfadc8829
emmamelkumian <147114444+emmamelkumian@users.noreply.github.com>
2025-01-31 19:22:20 -0800

    updated/complete email fetch interval design, with working count display

--- frontend/src/components/settings/settings.jsx
+++ frontend/src/components/settings/settings.jsx
@@ -29,25 +29,23 @@ function SummariesInInbox() {
 }
 
 function EmailFetchInterval() {
-  return (
-    <div className="settings-block email-fetch-interval">
-      <div className="header-container">
-        <h2 className="header">Email Fetch Interval</h2>
-        <p className="metric">(seconds)</p>
-      </div>
-      <input
-        className="slider"
-        type="range"
-        min="0"
-        max="600"
-        step="5"
-        onInput={inputEvent =>
-          (inputEvent.target.nextSibling.textContent = `${inputEvent.target.value}`)
-        }
-      />
-      <p className="count-display"> </p>
-    </div>
-  );
+    return (
+        <div className="settings-block email-fetch-interval">
+            <div className="header-container">
+                <h2 className="header">Email Fetch Interval</h2>
+                <p className="metric">(seconds)</p>
+            </div>
+            <input
+                className="slider"
+                type="range"
+                min="0"
+                max="600"
+                step="5"
+                onInput={(inputEvent) => inputEvent.target.nextSibling.textContent = `${inputEvent.target.value}`}
+            />
+            <p className="count-display"> </p>
+        </div>
+    );
 }
 
 function Theme() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Display from "./app";
 import "./preflight.css";
-import Display from "./Page";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
