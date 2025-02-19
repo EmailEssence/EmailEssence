059db0ca0922f753bff57afed8ea3ad8e225baf3
Joseph Madigan <jmad1112x@gmail.com>
2025-02-18 17:33:11 -0800

    reference

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Display from "./app";
 import "./preflight.css";
+import Display from "./page";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
